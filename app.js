1aa9f92729552eda99df002058d9b89887e1c96f
Rachel-2013 <rachelvrws20a1005@ssvminstitutions.ac.in>
2024-05-16 18:42:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function Greetings(){
+    var front = document.getElementById("front");
+    front.style.visibility="hidden";
+    var back = document.getElementById("back");
+    back.style.visibility="visible";
+}
